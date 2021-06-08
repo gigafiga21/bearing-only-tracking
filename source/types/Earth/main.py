fe75c0f4f4e8abacbe0d5835d1acf04a1ff0894a
gigafiga21 <gigafiga21@gmail.com>
2021-06-08 22:35:51 +0100

    WGS 84 Earth model description data (issue #2).

---
+++ source/types/Earth/main.py
@@ -0,0 +1,10 @@
+###
+# Info about model of the Earth (WGS 84)
+# @type {Dictionary}
+# @prop {Rfi} - smallest ellipsoid radius
+# @prop {Rla} - biggest ellipsoid radius
+##
+Earth = {
+    "Rfi": 6356752.3142,
+    "Rla": 6378137,
+}
